ad2b6acb8a8631cc6ab96979001b22b04428fe89
cristianodc <cristianocoffy@gmail.com>
2023-10-24 15:48:50 -0300

    Criação projeto

---
+++ controle_fin/src/main/java/br/com/controle_fin/ControleFinApplication.java
@@ -0,0 +1,13 @@
+package br.com.controle_fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinApplication.class, args);
+	}
+
+}
---
+++ controle_fin/src/test/java/br/com/controle_fin/ControleFinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controle_fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
