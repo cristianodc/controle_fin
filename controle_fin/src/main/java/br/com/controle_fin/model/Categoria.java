7cd20788c7db8178dd0fe02ad5a4eb839f8f41a8
cristianodc <cristianocoffy@gmail.com>
2023-11-09 17:15:20 -0300

    Criando class Categoria

---
+++ controle_fin/src/main/java/br/com/controle_fin/model/Categoria.java
@@ -0,0 +1,39 @@
+package br.com.controle_fin.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+@SequenceGenerator(name = "seq_cat",sequenceName = "seq_cat",allocationSize = 1,initialValue = 1)
+public class Categoria implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_cat")
+	private Long id;
+	private String descricao;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
